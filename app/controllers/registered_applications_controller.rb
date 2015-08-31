17d26d2e636441abc6558978d9c53ccdb2dadbf4
Jeremiah Shafer <jrshafer@mac.com>
2015-08-31 16:45:34 -0500

    Fixing something

---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,20 @@
+class RegisteredApplicationsController < ApplicationController
+  def index
+    @application = RegisteredApplication.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
