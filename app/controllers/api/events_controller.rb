75797c9cdb0d1340e4970d955686ee39dbf357f2
Jeremiah Shafer <jrshafer@mac.com>
2015-09-21 20:38:38 -0500

    Create event method fixed in API::EventsController

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,10 +2,6 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :set_access_control_headers
 
-  def index
-    events = Events.all
-  end
-
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
@@ -14,11 +10,20 @@ class API::EventsController < ApplicationController
 
   def create
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    event = registered_application.events.build( event_params ) if registered_application
+
+    if registered_application.nil?
+      render json: "Registered Application not found", status: :unprocessable_entity
+    elsif event.save
+      render json: event, status: :created
+    else
+      render json: event.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def event_params
-       params.permit(:name)
+    params.require(:event).permit(:url, :event_name, :ip_address)
   end
 end
